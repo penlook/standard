47b9eba6742faf1dd27c44340bddce67182c69a6
naviet <naviet2308@gmail.com>
2015-02-08 16:00:38 +0700

    Add coding convention for java

---
+++ main.java
@@ -0,0 +1,56 @@
+/**
+ *+--------------------------------------------------------------------------+
+ *| Penlook Project                                                          |
+ *+--------------------------------------------------------------------------+
+ *| Copyright (c) 2015 Penlook Development Team                              |
+ *+--------------------------------------------------------------------------+
+ *|                                                                          |
+ *| This program is free software: you can redistribute it and/or modify	 |
+ *| it under the terms of the GNU Affero General Public License as           |
+ *| published by the Free Software Foundation, either version 3 of the       |
+ *| License, or (at your option) any later version.                          |
+ *|                                                                          |
+ *| This program is distributed in the hope that it will be useful, but	     |
+ *| WITHOUT ANY WARRANTY; without even the implied warranty of               |
+ *| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            |
+ *| GNU Affero General Public License for more details.                      |
+ *|                                                                          |
+ *| You should have received a copy of the GNU Affero General Public License |
+ *| along with this program.  If not, see <http://www.gnu.org/licenses/>.    |
+ *|                                                                          |
+ *+--------------------------------------------------------------------------+
+ */
+ 
+/**
+ * Calculate the area of a circle & print out screen
+ *   
+ * @author  Vietna@penlook.com
+ * @version 1.0
+ * @since   08-02-2015 
+ */
+
+class Main {
+  static final float	PI = 3.14;    // number PI
+
+  /**
+   * The main program.
+   *
+   * @param    args		command line arguments (ignored)
+   * @author			Vietna@penlook.com
+   */
+  public static void main( String [] args ) {
+	int radius = 5;
+    System.out.println("Value of PI is ", areaCircle(radius));
+  }
+  
+  /**
+   * Calculate the area of a circle
+   *
+   * @param		Int radius		radius of the circle
+   * @return					area of the circle
+   * @author					Vietna@penlook.com
+   */
+  public void areaCircle(Int radius){
+	  return PI * radius * radius;
+  }
+}
