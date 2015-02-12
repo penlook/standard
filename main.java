31eec0809b123a112b16fd2d86b70755d20907f6
namvh <vhnam2504@gmail.com>
2015-02-12 15:18:10 +0700

    update Java, JavaScript, PHP and Zephir commentary.

--- main.java
+++ main.java
@@ -1,30 +1,36 @@
-/*
- +--------------------------------------------------------------------------+
- | Penlook Project                                                          |
- +--------------------------------------------------------------------------+
- | Copyright (c) 2015 Penlook Development Team                              |
- +--------------------------------------------------------------------------+
- |                                                                          |
- | This program is free software: you can redistribute it and/or modify     |
- | it under the terms of the GNU Affero General Public License as           |
- | published by the Free Software Foundation, either version 3 of the       |
- | License, or (at your option) any later version.                          |
- |                                                                          |
- | This program is distributed in the hope that it will be useful, but      |
- | WITHOUT ANY WARRANTY; without even the implied warranty of               |
- | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            |
- | GNU Affero General Public License for more details.                      |
- |                                                                          |
- | You should have received a copy of the GNU Affero General Public License |
- | along with this program.  If not, see <http://www.gnu.org/licenses/>.    |
- |                                                                          |
- +--------------------------------------------------------------------------+
- |   Authors: Loi Nguyen  <loint@penlook.com>                               |
- |            Tin Nguyen  <tinntt@penlook.com>                              |
- |            Nam Vo      <namvh@penlook.com>                               |
- |            Viet Nguyen <vietna@penlook.com>                              |
- +--------------------------------------------------------------------------+
-*/
+/**
+ *
+ * Penlook Project
+ *
+ * Copyright (c) 2015 Penlook Development Team
+ *
+ *
+ * --------------------------------------------------------------------
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ * --------------------------------------------------------------------
+ *
+ *
+ * Authors:
+ *     Loi Nguyen       <loint@penlook.com>
+ *     Tin Nguyen       <tinntt@penlook.com>
+ *     Nam Vo           <namvh@penlook.com>
+ *     Viet Nguyen      <vietna@penlook.com>
+ *
+ */
 
 /**
  * Calculate the area of a circle & print out screen
@@ -33,29 +39,26 @@
  * @version 1.0
  * @since   Class available since Release 1.0
  */
-
 class Main {
-  static final float	PI = 3.14;    // number PI
+    static final float	PI = 3.14;    // number PI
 
-  /**
-   * The main program.
-   *
-   * @param    args		command line arguments (ignored)
-   * @author			vietna@penlook.com
-   */
-  public static void main( String [] args ) {
-	int radius = 5;
-    System.out.println("Value of area is ", areaCircle(radius));
-  }
+    /**
+    * The main program.
+    *
+    * @param    args    command line arguments (ignored)
+    */
+    public static void main( String [] args ) {
+        int radius = 5;
+        System.out.println("Value of area is ", areaCircle(radius));
+    }
 
-  /**
-   * Calculate the area of a circle
-   *
-   * @param		Int radius		radius of the circle
-   * @return					area of the circle
-   * @author					vietna@penlook.com
-   */
-  public void areaCircle(Int radius){
-	  return PI * radius * radius;
-  }
+    /**
+    * Calculate the area of a circle
+    *
+    * @param    Int     radius of the circle
+    * @return			area of the circle
+    */
+    public void areaCircle(Int radius){
+        return PI * radius * radius;
+    }
 }
--- main.zep
+++ main.zep
@@ -1,30 +1,36 @@
-/*
- +--------------------------------------------------------------------------+
- | Penlook Project                                                          |
- +--------------------------------------------------------------------------+
- | Copyright (c) 2015 Penlook Development Team                              |
- +--------------------------------------------------------------------------+
- |                                                                          |
- | This program is free software: you can redistribute it and/or modify	    |
- | it under the terms of the GNU Affero General Public License as           |
- | published by the Free Software Foundation, either version 3 of the       |
- | License, or (at your option) any later version.                          |
- |                                                                          |
- | This program is distributed in the hope that it will be useful, but	    |
- | WITHOUT ANY WARRANTY; without even the implied warranty of               |
- | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            |
- | GNU Affero General Public License for more details.                      |
- |                                                                          |
- | You should have received a copy of the GNU Affero General Public License |
- | along with this program.  If not, see <http://www.gnu.org/licenses/>.    |
- |                                                                          |
- +--------------------------------------------------------------------------+
- |   Authors: Loi Nguyen  <loint@penlook.com>                               |
- |            Tin Nguyen  <tinntt@penlook.com>                              |
- |            Nam Vo      <namvh@penlook.com>                               |
- |            Viet Nguyen <vietna@penlook.com>                              |
- +--------------------------------------------------------------------------+
-*/
+/**
+ *
+ * Penlook Project
+ *
+ * Copyright (c) 2015 Penlook Development Team
+ *
+ *
+ * --------------------------------------------------------------------
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ * --------------------------------------------------------------------
+ *
+ *
+ * Authors:
+ *     Loi Nguyen       <loint@penlook.com>
+ *     Tin Nguyen       <tinntt@penlook.com>
+ *     Nam Vo           <namvh@penlook.com>
+ *     Viet Nguyen      <vietna@penlook.com>
+ *
+ */
 
 namespace App\Controller;
 
@@ -44,23 +50,22 @@ use App\Controller;
 class MainController extends Controller
 {
 
-	/**
-	 * Application
-	 *
-	 * @var App\App This is a sample about property
-	 */
-	public app;
+    /**
+    * Application
+    *
+    * @var App\App This is a sample about property
+    */
+    public app;
 
-	/**
-	 * Index Action
-	 * This is a sample about action
-	 *
-	 * @author Loi Nguyen <loint@penlook.com>
-	 * @return string
-	 */
-	public inline function indexAction()
-	{
-		return "Sample string";
-	}
-
+    /**
+    * Index Action
+    * This is a sample about action
+    *
+    * @author Loi Nguyen <loint@penlook.com>
+    * @return string
+    */
+    public inline function indexAction()
+    {
+        return "Sample string";
+    }
 }
